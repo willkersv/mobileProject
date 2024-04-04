c09e7e447b9e85e54843174e9c74a681eeac4929
willkersv <willkersv@gmail.com>
2024-04-04 16:43:47 -0300

    base gerada pelo expo

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Base do projeto apenas para subir no git</Text>
       <StatusBar style="auto" />
     </View>
   );
