1b2386ff8e70e0dc309f7ddce8db799492b95581
Lucas-Munhoz <dev.lucasandremc@gmail.com>
2024-04-14 23:47:04 -0300

    starting
    Primeiras tentativas
    Acabei esquecendo que o professor tinha passado um layout especifico, mas ja comecei a mudar oq fiz, para ficar mais parecido com o do professor

--- App.js
+++ App.js
@@ -1,22 +1,73 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet, Text, TextInput, TouchableOpacity } from 'react-native';
+import { useState } from 'react';
+import Botao from './src/components/Botao.js';
+import ClickableText from './src/components/ClickableText.js'
 
-export default function App() {
+const App = () => {
+
+  const [txtEmail, setTxtEmail] = useState('')
+  const [txtSenha, setTxtSenha] = useState('')
+
+  const compareLogin = () => {
+    let Email = txtEmail
+    let senha = txtSenha
+
+    if(Email === "adm" && senha === "123"){
+      console.log("Acesso concedido")
+    }
+    console.log("Email = " + Email + "\n Senha = " + senha)
+  }
+
+  const toScreenForgetPsw = () => {
+    console.log("Direcionado para a tela ESQUECEU SENHA")
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style = {styles.logo}>Satisfying.you</Text>
+
+      <Text style = {styles.text}>Email</Text>
+      <TextInput style = {styles.textInput} value={txtEmail} onChangeText={setTxtEmail}/>
+
+      <Text style = {styles.text}>Senha</Text>
+      <TextInput style = {styles.textInput} value={txtSenha} onChangeText={setTxtSenha}/>
+
+      <Botao txtButton="Entrar" buttonColor="#419ED7" functionButton={compareLogin}/>
+
+      <ClickableText txt="Esqueci minha senha" txtColor="#FF6432" functionTextClick={toScreenForgetPsw}/>
+
+      <StatusBar style="auto"/>
     </View>
   );
 }
 
-//Teste Willker tchola
-
 const styles = StyleSheet.create({
+  logo: {
+    fontWeight: "bold",
+    fontSize: 36,
+    padding: 20,
+    color: "#4169e1"
+  },
+  text: {
+    textAlign: "left",
+    justifyContent: "flex-start",
+    fontSize: 20,
+    padding: 10
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#eee",
+    width: 200,
+    height: 35,
+    paddingLeft: 5
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#372775',
+    alignItems: "center",
+    justifyContent: 'center'
   },
 });
+
+export default App
