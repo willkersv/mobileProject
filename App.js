fb25a2ab2375b8b17f6fac910bb4f9d7adfd8e17
Arthur Massuia Miranda <111662777+ArthurM09@users.noreply.github.com>
2024-04-18 23:24:02 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
-
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from "@react-navigation/stack"
 import Login from "./src/screens/Login"
+import NovaConta from './src/screens/NovaConta';
 
 const Stack = createStackNavigator()
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Login" screenOptions={{ headerStyle: {backgroundColor: '#2B1D62', height: 70}, headerTitle: 'Nova Conta', headerTintColor: '#573fba', headerTitleStyle: {color: '#FFFFFF', fontSize: 30, fontFamily: 'AveriaLibre'} }}>
+
         <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/>
+        <Stack.Screen name="NovaConta" component={NovaConta} />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
