010699581e0eb6d23c3dfb5b555762dba4dedf6c
Felipe-Alves-Cerquiare <felipe.cerquiares.alves@gmail.com>
2024-04-22 23:37:58 -0300

    Atualização

--- App.js
+++ App.js
@@ -10,26 +10,31 @@ import Agradecimentos from "./src/screens/Agradecimentos";
 import NovaConta from "./src/screens/NovaConta.js";
 import ModificarPesquisa from './src/screens/ModificarPesquisa';
 import Relatorio from './src/screens/Relatorio';
+// import RecuperarSenha from './src/screens/RecuperarSenha';
+// import NovaPesquisa from './src/screens/NovaPesquisa';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Login"
-        screenOptions={{ headerShown: false }}
-      >
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Drawer" component={Drawer} />
-        <Stack.Screen name="AcoesPesquisas" component={AcoesPesquisas} />
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Coleta" component={Coleta} />
-        <Stack.Screen name="Agradecimentos" component={Agradecimentos} />
-        <Stack.Screen name="NovaConta" component={NovaConta} />
-        <Stack.Screen name="ModificarPesquisa" component={ModificarPesquisa}/>
-        <Stack.Screen name="Relatorio" component={Relatorio}/>
-        
+      <Stack.Navigator initialRouteName="Login" 
+      screenOptions={{ headerStyle: {backgroundColor: '#2B1D62', height: 80}, 
+      headerTintColor: '#573fba', 
+      headerTitleStyle: {color: '#FFFFFF', fontSize: 30, fontFamily: 'AveriaLibre'} }}>
+
+        <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+        <Stack.Screen name="Drawer" component={Drawer} options={{ headerShown: false }} />
+        <Stack.Screen name="Carnaval" component={AcoesPesquisas} options={{ headerShown: true }} />
+        <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+        <Stack.Screen name="Coleta" component={Coleta} options={{ headerShown: false }} />
+        <Stack.Screen name="Agradecimentos" component={Agradecimentos} options={{ headerShown: false }} />
+        <Stack.Screen name="Nova Conta" component={NovaConta} options={{ headerShown: true }} />
+        <Stack.Screen name="Modificar Pesquisa" component={ModificarPesquisa} options={{ headerShown: true }}/>
+        <Stack.Screen name="Relatório" component={Relatorio} options={{ headerShown: true }}/>
+        {/* <Stack.Screen name="RecuperarSenha" component={RecuperarSenha} options={{ headerShown: true }}/>
+        <Stack.Screen name="NovaPesquisa" component={NovaPesquisa} options={{ headerShown: true }}/> */}
+
       </Stack.Navigator>
     </NavigationContainer>
   );
